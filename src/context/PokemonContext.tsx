737e4039a1ff7c950c0714c700056b3bd9c60147
Guilherme Mattos da Silva <91551718+guimattos91@users.noreply.github.com>
2023-09-27 00:02:30 -0300

    Exporting GET_POKEMONS_QUERY

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -11,6 +11,7 @@ import {
 
 import {
   ApolloQueryResult,
+  DocumentNode,
   LazyQueryExecFunction,
   OperationVariables,
   useLazyQuery,
@@ -38,6 +39,8 @@ interface IContextProps {
   ) => Promise<ApolloQueryResult<PokemonsQueryResultDataType>>
   setSearchPokemons: Dispatch<SetStateAction<string>>
   searchPokemons: string
+  GET_POKEMONS_QUERY: DocumentNode
+  GET_POKEMON_QUERY: DocumentNode
 }
 
 interface IPokemonProviderProps {
@@ -130,6 +133,8 @@ export const PokemonProvider: React.FC<IPokemonProviderProps> = ({
           pokemon,
           hasMorePages,
           searchPokemons,
+          GET_POKEMONS_QUERY,
+          GET_POKEMON_QUERY,
           fetchNextPage,
           fetchPokemon,
           refetchPokemons,
